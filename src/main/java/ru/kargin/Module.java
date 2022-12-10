24df38ebb1af8bbdad5d50efacf5662531de0c3a
Aleksandr Kargin <AKargin@korusconsulting.ru>
2022-12-10 21:30:43 +0300

    test 1

--- src/main/java/ru/kargin/Module.java
+++ src/main/java/ru/kargin/Module.java
@@ -1,10 +1,24 @@
 package ru.kargin;
 
 public class Module {
+
+    private double x1;
+    private double x2;
+    private double a;
+    private double b;
+    private double c;
     public static void main(String[] args){
-        int a = 10;
     }
-    public int intFoo(){
-        return 3;
+
+    public Module(double a, double b, double c){
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+    public double[] solve(){
+        //double d = this.b*this.b - 4*this.a*this.c;
+        double x1 = 0;
+        double x2 = 0;
+        return new double[]{x1,x2};
     }
 }
--- src/test/java/ru/kargin/ModuleTest.java
+++ src/test/java/ru/kargin/ModuleTest.java
@@ -3,16 +3,15 @@ package ru.kargin;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ModuleTest {
 
     @Test
-    public void main() {
-    }
-
-    @Test
-    public void intFoo() {
-        Module a = new Module();
-        assertEquals(3,a.intFoo());
+    public void test2(){
+        //Написать тест, который проверяет, что для уравнения x^2+1 = 0 корней нет (возвращается пустой массив)
+        Module obj = new Module(1,0,1);
+        double compare = 0;
+        assertArrayEquals(new double[]{0,0}, obj.solve(), compare);
     }
 }
