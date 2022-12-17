5109ffec997f1d807961e2016c62587aa9b1376b
Aleksandr Kargin <AKargin@korusconsulting.ru>
2022-12-17 19:44:47 +0300

    test 5

--- src/test/java/ru/kargin/ModuleTest.java
+++ src/test/java/ru/kargin/ModuleTest.java
@@ -38,4 +38,12 @@ public class ModuleTest {
         Module obj = new Module(0,2,1);
         obj.solve();
     }
+
+    @Test
+    public void test3_2()  throws ArithmeticException{
+        //Написать тест, который проверяет, что для уравнения x^2+2x+1 = 0 есть один корень кратности 2 (x1= x2 = -1).
+        Module obj = new Module(1,2.000001,1);
+        double compare = 0;
+        assertArrayEquals(new double[]{-1,-1}, obj.solve(), compare);
+    }
 }
